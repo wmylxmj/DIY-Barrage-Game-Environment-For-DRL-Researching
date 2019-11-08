bd38028573faa5104113d9ed6307601bba6997b6
wmy <1397322329@qq.com>
2019-11-08 15:36:50 +0800

    Add files via upload

---
+++ aliens.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  5 16:01:24 2019
+
+@author: wmy
+"""
+
+import pygame
+from config import GameConfigure
+from pygame.sprite import Sprite
+import random
+import math
+from effect import BoomEffect
+from pygame.sprite import Group
+
+class Alien(Sprite, GameConfigure):
+    
+    def __init__(self, window, target, resource):
+        Sprite.__init__(self)
+        GameConfigure.__init__(self)
+        self.window = window
+        self.target = target
+        self.resource = resource
+        self.image = pygame.image.load(resource)
+        self.rect = self.image.get_rect()
+        # fire init
+        self.fireCount = 0
+        self.fireInterval = 75
+        # number of lives
+        self.hp = 10
+        # active flag
+        self.activeFlag = False
+        # boom effect
+        self.boomEffect = BoomEffect(self.window)
+        # bullets
+        self.bullets = Group()
+        pass
+    
+    def update(self):
+        # boom effect
+        if self.hp <= 0:
+            self.boomEffect.setCenter(self.rect.centerx, self.rect.centery)
+            self.boomEffect.startFlag = True
+            pass
+        self.boomEffect.update()
+        pass
+    
+    def blitme(self):
+        # blite alien
+        if self.activeFlag and self.hp > 0:
+            self.window.blit(self.image, self.rect)
+            pass
+        # boom effect
+        self.boomEffect.blitme()
+        if self.activeFlag:
+            self.window.blit(self.image, self.rect)
+            pass
+        pass
+        
+    pass
+
+
+class AlienBullet(Sprite, GameConfigure):
+    
+    def __init__(self, alien, resource, decisionPointResource):
+        Sprite.__init__(self)
+        GameConfigure.__init__(self)
+        self.alien = alien
+        self.target = self.alien.target
+        self.resource = resource
+        # init image
+        self.image = pygame.image.load(resource)
+        self.imageRect = self.image.get_rect()
+        self.window = self.alien.window
+        self.decisionPointResource = decisionPointResource
+        # init decision point
+        self.decisionPoint = pygame.image.load(decisionPointResource)
+        self.rect = self.decisionPoint.get_rect()
+        pass
+    
+    def update(self):
+        if self.imageRect.top >= self.windowHeight or \
+        self.imageRect.right <= 0 or self.imageRect.left >= self.windowWidth or \
+        self.imageRect.bottom <= 0:
+            self.alien.bullets.remove(self)
+            pass
+        pass
+    
+    def blitme(self):
+        self.window.blit(self.image, self.imageRect)
+        pass
+    
+    pass
+
---
+++ game.py
@@ -0,0 +1,203 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  3 15:37:06 2019
+
+@author: wmy
+"""
+
+import sys
+import pygame
+from pygame.sprite import Group
+from pygame.sprite import Sprite
+import random
+import numpy as np
+import math
+import time
+from config import GameConfigure
+from background import GameBackground
+from stars import StarGroup
+from ship import Ship
+from stages.stage1 import Stage1
+
+class AlienGame(GameConfigure):
+    
+    def __init__(self):
+        GameConfigure.__init__(self)
+        pygame.init()
+        pygame.mixer.init()
+        pygame.display.set_caption("Alien Invasion")
+        self.window = pygame.display.set_mode((self.windowWidth, self.windowHeight))
+        self.background = GameBackground(self.window)
+        self.stars = StarGroup(self.window)
+        self.ship = Ship(self.window)
+        self.stages = [Stage1(self.window, self.ship)]
+        self.stageSkipCount = 0
+        pass
+    
+    def eventsProcess(self):
+        '''事件处理'''
+        for event in pygame.event.get():                
+            if event.type == pygame.QUIT:
+                pygame.mixer.music.stop()
+                self.musicPlayFlag = False
+                sys.exit()
+                pass
+            pass
+        if self.AIMode == False:
+            pressedKeys = pygame.key.get_pressed()
+            # right
+            if pressedKeys[pygame.K_RIGHT] and self.ship.aliveFlag:
+                self.ship.movingRight = True
+                pass
+            else:
+                self.ship.movingRight = False
+                pass
+            # left
+            if pressedKeys[pygame.K_LEFT] and self.ship.aliveFlag:
+                self.ship.movingLeft = True
+                pass
+            else:
+                self.ship.movingLeft = False
+                pass
+            # up
+            if pressedKeys[pygame.K_UP] and self.ship.aliveFlag:
+                self.ship.movingUp = True
+                pass
+            else:
+                self.ship.movingUp = False
+                pass
+            # down
+            if pressedKeys[pygame.K_DOWN] and self.ship.aliveFlag:
+                self.ship.movingDown = True
+                pass
+            else:
+                self.ship.movingDown = False
+                pass
+            # shoot
+            if pressedKeys[pygame.K_z] and self.ship.aliveFlag:
+                self.ship.fireFlag = True
+                pass
+            else:
+                self.ship.fireFlag = False
+                pass
+            # slow down
+            if pressedKeys[pygame.K_LSHIFT] and self.ship.aliveFlag:
+                self.ship.speed = self.shipSlowSpeed
+                pass
+            else:
+                self.ship.speed = self.shipNormalSpeed
+                pass
+            # restart
+            if pressedKeys[pygame.K_SPACE]  and self.ship.aliveFlag==False:
+                self.ship.aliveFlag = True
+                self.ship.boomEffect.reset()
+                if len(self.stages) != 0:
+                    for alien in self.stages[0].aliens: 
+                        for bullet in alien.bullets:
+                            if ((bullet.rect.centerx - self.ship.rect.centerx) ** 2 + \
+                            (bullet.rect.centery - self.ship.rect.centery)) < 150**2:                         
+                                alien.bullets.remove(bullet)
+                                pass
+                            pass
+                        pass
+                    for bullet in self.stages[0].bulletsRemains:
+                        if ((bullet.rect.centerx - self.ship.rect.centerx) ** 2 + \
+                        (bullet.rect.centery - self.ship.rect.centery)) < 150**2:
+                            self.stages[0].bulletsRemains.remove(bullet)
+                            pass
+                        pass
+                    pass
+                pass
+            pass
+        pass
+    
+    def computeDamage(self):
+        '''计算伤害'''
+        if len(self.stages) == 0:
+            return
+        crashed = pygame.sprite.groupcollide(self.stages[0].aliens, self.ship.bullets, False, True)   
+        for alien in self.stages[0].aliens:
+            try:
+                crashed[alien]
+                pass
+            except:
+                pass
+            else:
+                bullets = crashed[alien]
+                for bullet in bullets:
+                    alien.hp -= bullet.attack
+                    pass
+                pass
+            collideList = pygame.sprite.spritecollide(self.ship, alien.bullets, True)
+            if len(collideList) != 0:
+                self.ship.aliveFlag = False
+                pass
+            pass
+        collideList = pygame.sprite.spritecollide(self.ship, self.stages[0].bulletsRemains, True)
+        if len(collideList) != 0:
+            self.ship.aliveFlag = False
+            pass
+        for battery in self.ship.batterys:
+            crashed = pygame.sprite.groupcollide(self.stages[0].aliens, battery.bullets, False, True)   
+            for alien in self.stages[0].aliens:
+                try:
+                    crashed[alien]
+                    pass
+                except:
+                    pass
+                else:
+                    bullets = crashed[alien]
+                    for bullet in bullets:
+                        alien.hp -= bullet.attack
+                        pass
+                    pass
+                pass
+            pass
+        pass
+    
+    def stagesRun(self):
+        for stage in self.stages:
+            if stage.aliensCreated == False:
+                stage.creatAliens()
+                pass
+            pass
+        if len(self.stages) != 0:
+            self.stages[0].run()
+            if self.stages[0].isDone() == False:
+                if self.stages[0].aliensActived == False:
+                    self.stages[0].activeAliens()
+                    pass
+                pass
+            else:  
+                self.stageSkipCount += 1
+                if self.stageSkipCount > 500:
+                    self.stages.remove(self.stages[0])
+                    self.stageSkipCount = 0
+                    pass
+                pass
+            pass
+        pass
+    
+    def run(self):
+        '''运行游戏'''
+        while True:
+            self.background.run()
+            self.stars.run()
+            self.stagesRun()
+            self.ship.run()
+            self.computeDamage()
+            self.eventsProcess()  
+            if self.ship.aliveFlag == False:
+                time.sleep(0.1)
+                pass
+            pygame.display.flip()  
+            pass
+        pass
+    
+    pass
+    
+        
+if __name__ == "__main__":
+    game = AlienGame()
+    game.run()
+    pass
